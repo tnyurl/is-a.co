bffc7a51dbf01540d263b8c5b8c8511578a90924
tnyurl <98256557+tnyurl@users.noreply.github.com>
2023-04-07 14:22:29 +1000

    Update and rename portfolio.is-a.co.js to discord.is-a.co.js

---
+++ domains/discord.is-a.co.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Discord Subdomain',
+  domain: 'is-a.co',
+  subdomain: 'discord',
+  owner: {
+    repo: 'https://github.com/tnyurl/is-a.co',
+    email: 'kittykay2010@outlook.com',
+  },
+  record: {
+    NS: 'ns1.byet.org', 'ns2.byet.org', 'ns3.byet.org', 'ns4.byet.org', 'ns5.byet.org',
+  },
+  proxy: false,
+})
